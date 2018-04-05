cad3b7f1822d16c83751440c574f33644e77d0f8
SeventhQueen <seventhqueen@users.noreply.github.com>
2018-04-05 19:01:46 +0300

    Metadata improvement
    Update image meta data on image creation to support 3rd party image optimisation plugins

--- aq_resizer.php
+++ aq_resizer.php
@@ -9,11 +9,11 @@
  * License       : WTFPL - http://sam.zoy.org/wtfpl/
  * Documentation : https://github.com/sy4mil/Aqua-Resizer/
  *
- * @param string  $url      - (required) must be uploaded using wp media uploader
- * @param int     $width    - (required)
- * @param int     $height   - (optional)
- * @param bool    $crop     - (optional) default to soft crop
- * @param bool    $single   - (optional) returns an array if false
+ * @param string $url - (required) must be uploaded using wp media uploader
+ * @param int $width - (required)
+ * @param int $height - (optional)
+ * @param bool $crop - (optional) default to soft crop
+ * @param bool $single - (optional) returns an array if false
  * @param bool    $upscale  - (optional) resizes smaller images
  * @uses  wp_upload_dir()
  * @uses  image_resize_dimensions()
@@ -22,227 +22,281 @@
  * @return str|array
  */
 
-if(!class_exists('Aq_Resize')) {
-    class Aq_Exception extends Exception {}
+if ( ! class_exists( 'Aq_Resize' ) ) {
+	class Aq_Exception extends Exception {}
 
-    class Aq_Resize
-    {
-        /**
-         * The singleton instance
-         */
-        static private $instance = null;
+	class Aq_Resize {
+		/**
+		 * The singleton instance
+		 */
+		static private $instance = null;
 
-        /**
-         * Should an Aq_Exception be thrown on error?
-         * If false (default), then the error will just be logged.
-         */
-        public $throwOnError = false;
+		/**
+		 * Should an Aq_Exception be thrown on error?
+		 * If false (default), then the error will just be logged.
+		 */
+		public $throwOnError = false;
 
-        /**
-         * No initialization allowed
-         */
-        private function __construct() {}
+		/**
+		 * No initialization allowed
+		 */
+		private function __construct() {
+		}
 
-        /**
-         * No cloning allowed
-         */
-        private function __clone() {}
+		/**
+		 * No cloning allowed
+		 */
+		private function __clone() {
+		}
 
-        /**
-         * For your custom default usage you may want to initialize an Aq_Resize object by yourself and then have own defaults
-         */
-        static public function getInstance() {
-            if(self::$instance == null) {
-                self::$instance = new self;
-            }
+		/**
+		 * For your custom default usage you may want to initialize an Aq_Resize object by yourself and then have own defaults
+		 */
+		static public function getInstance() {
+			if ( self::$instance == null ) {
+				self::$instance = new self;
+			}
 
-            return self::$instance;
-        }
+			return self::$instance;
+		}
 
-        /**
-         * Run, forest.
-         */
-        public function process( $url, $width = null, $height = null, $crop = null, $single = true, $upscale = false ) {
-            try {
-                // Validate inputs.
-                if (!$url)
-                    throw new Aq_Exception('$url parameter is required');
-                if (!$width)
-                    throw new Aq_Exception('$width parameter is required');
+		/**
+		 * Run, forest.
+		 */
+		public function process( $url, $width = null, $height = null, $crop = null, $single = true, $upscale = false ) {
+			try {
+				// Validate inputs.
+				if ( ! $url || ( ! $width && ! $height ) ) {
+					return false;
+				}
 
-                // Caipt'n, ready to hook.
-                if ( true === $upscale ) add_filter( 'image_resize_dimensions', array($this, 'aq_upscale'), 10, 6 );
+				// Caipt'n, ready to hook.
+				if ( true === $upscale ) {
+					add_filter( 'image_resize_dimensions', array( $this, 'aq_upscale' ), 10, 6 );
+				}
 
-                // Define upload path & dir.
-                $upload_info = wp_upload_dir();
-                $upload_dir = $upload_info['basedir'];
-                $upload_url = $upload_info['baseurl'];
-                
-                $http_prefix = "http://";
-                $https_prefix = "https://";
-                $relative_prefix = "//"; // The protocol-relative URL
-                
-                /* if the $url scheme differs from $upload_url scheme, make them match 
-                   if the schemes differe, images don't show up. */
-                if(!strncmp($url,$https_prefix,strlen($https_prefix))){ //if url begins with https:// make $upload_url begin with https:// as well
-                    $upload_url = str_replace($http_prefix,$https_prefix,$upload_url);
-                }
-                elseif(!strncmp($url,$http_prefix,strlen($http_prefix))){ //if url begins with http:// make $upload_url begin with http:// as well
-                    $upload_url = str_replace($https_prefix,$http_prefix,$upload_url);      
-                }
-                elseif(!strncmp($url,$relative_prefix,strlen($relative_prefix))){ //if url begins with // make $upload_url begin with // as well
-                    $upload_url = str_replace(array( 0 => "$http_prefix", 1 => "$https_prefix"),$relative_prefix,$upload_url);
-                }
-                
+				// Define upload path & dir.
+				$upload_info = wp_upload_dir();
+				$upload_dir  = $upload_info['basedir'];
+				$upload_url  = $upload_info['baseurl'];
 
-                // Check if $img_url is local.
-                if ( false === strpos( $url, $upload_url ) )
-                    throw new Aq_Exception('Image must be local: ' . $url);
+				$http_prefix  = "http://";
+				$https_prefix = "https://";
+				$relative_prefix = "//"; // The protocol-relative URL
 
-                // Define path of image.
-                $rel_path = str_replace( $upload_url, '', $url );
-                $img_path = $upload_dir . $rel_path;
+				/* if the $url scheme differs from $upload_url scheme, make them match
+				   if the schemes differe, images don't show up. */
+				if ( ! strncmp( $url, $https_prefix, strlen( $https_prefix ) ) ) { //if url begins with https:// make $upload_url begin with https:// as well
+					$upload_url = str_replace( $http_prefix, $https_prefix, $upload_url );
+				} elseif ( ! strncmp( $url, $http_prefix, strlen( $http_prefix ) ) ) { //if url begins with http:// make $upload_url begin with http:// as well
+					$upload_url = str_replace( $https_prefix, $http_prefix, $upload_url );
+				}
+				elseif(!strncmp($url,$relative_prefix,strlen($relative_prefix))){ //if url begins with // make $upload_url begin with // as well
+					$upload_url = str_replace(array( 0 => "$http_prefix", 1 => "$https_prefix"),$relative_prefix,$upload_url);
+				}
 
-                // Check if img path exists, and is an image indeed.
-                if ( ! file_exists( $img_path ) or ! getimagesize( $img_path ) )
-                    throw new Aq_Exception('Image file does not exist (or is not an image): ' . $img_path);
 
-                // Get image info.
-                $info = pathinfo( $img_path );
-                $ext = $info['extension'];
-                list( $orig_w, $orig_h ) = getimagesize( $img_path );
+				// Check if $img_url is local.
+				if ( false === strpos( $url, $upload_url ) ) {
+					throw new Aq_Exception('Image must be local: ' . $url);
+				}
 
-                // Get image size after cropping.
-                $dims = image_resize_dimensions( $orig_w, $orig_h, $width, $height, $crop );
-                $dst_w = $dims[4];
-                $dst_h = $dims[5];
+				// Define path of image.
+				$rel_path = str_replace( $upload_url, '', $url );
+				$img_path = $upload_dir . $rel_path;
 
-                // Return the original image only if it exactly fits the needed measures.
-                if ( ! $dims || ( ( ( null === $height && $orig_w == $width ) xor ( null === $width && $orig_h == $height ) ) xor ( $height == $orig_h && $width == $orig_w ) ) ) {
-                    $img_url = $url;
-                    $dst_w = $orig_w;
-                    $dst_h = $orig_h;
-                } else {
-                    // Use this to check if cropped image already exists, so we can return that instead.
-                    $suffix = "{$dst_w}x{$dst_h}";
-                    $dst_rel_path = str_replace( '.' . $ext, '', $rel_path );
-                    $destfilename = "{$upload_dir}{$dst_rel_path}-{$suffix}.{$ext}";
+				// Check if img path exists, and is an image indeed.
+				if ( ! file_exists( $img_path ) or ! getimagesize( $img_path ) ) {
+					throw new Aq_Exception( 'Image file does not exist (or is not an image): ' . $img_path );
+				}
 
-                    if ( ! $dims || ( true == $crop && false == $upscale && ( $dst_w < $width || $dst_h < $height ) ) ) {
-                        // Can't resize, so return false saying that the action to do could not be processed as planned.
-                        throw new Aq_Exception('Unable to resize image because image_resize_dimensions() failed');
-                    }
-                    // Else check if cache exists.
-                    elseif ( file_exists( $destfilename ) && getimagesize( $destfilename ) ) {
-                        $img_url = "{$upload_url}{$dst_rel_path}-{$suffix}.{$ext}";
-                    }
-                    // Else, we resize the image and return the new resized image url.
-                    else {
+				// Get image info.
+				$info = pathinfo( $img_path );
+				$ext  = $info['extension'];
+				list( $orig_w, $orig_h ) = getimagesize( $img_path );
 
-                        $editor = wp_get_image_editor( $img_path );
+				// Get image size after cropping.
+				$dims  = image_resize_dimensions( $orig_w, $orig_h, $width, $height, $crop );
+				$dst_w = $dims[4];
+				$dst_h = $dims[5];
 
-                        if ( is_wp_error( $editor ) || is_wp_error( $editor->resize( $width, $height, $crop ) ) ) {
-                            throw new Aq_Exception('Unable to get WP_Image_Editor: ' . 
-                                                   $editor->get_error_message() . ' (is GD or ImageMagick installed?)');
-                        }
+				// Return the original image only if it exactly fits the needed measures.
+				if ( ! $dims && ( ( ( null === $height && $orig_w == $width ) xor ( null === $width && $orig_h == $height ) ) xor ( $height == $orig_h && $width == $orig_w ) ) ) {
+					$img_url = $url;
+					$dst_w   = $orig_w;
+					$dst_h   = $orig_h;
+				} else {
+					// Use this to check if cropped image already exists, so we can return that instead.
+					$suffix       = "{$dst_w}x{$dst_h}";
+					$dst_rel_path = str_replace( '.' . $ext, '', $rel_path );
+					$destfilename = "{$upload_dir}{$dst_rel_path}-{$suffix}.{$ext}";
 
-                        $resized_file = $editor->save();
+					if ( ! $dims || ( true == $crop && false == $upscale && ( $dst_w < $width || $dst_h < $height ) ) ) {
+						// Can't resize, so return false saying that the action to do could not be processed as planned.
+						throw new Aq_Exception('Unable to resize image because image_resize_dimensions() failed');
+					}
+					// Else check if cache exists.
+					elseif ( file_exists( $destfilename ) && getimagesize( $destfilename ) ) {
+						$img_url = "{$upload_url}{$dst_rel_path}-{$suffix}.{$ext}";
+					} // Else, we resize the image and return the new resized image url.
+					else {
 
-                        if ( ! is_wp_error( $resized_file ) ) {
-                            $resized_rel_path = str_replace( $upload_dir, '', $resized_file['path'] );
-                            $img_url = $upload_url . $resized_rel_path;
-                        } else {
-                            throw new Aq_Exception('Unable to save resized image file: ' . $editor->get_error_message());
-                        }
+						$editor = wp_get_image_editor( $img_path );
 
-                    }
-                }
+						if ( is_wp_error( $editor ) || is_wp_error( $editor->resize( $width, $height, $crop ) ) ) {
+							throw new Aq_Exception('Unable to get WP_Image_Editor: ' .
+							                       $editor->get_error_message() . ' (is GD or ImageMagick installed?)');
+						}
 
-                // Okay, leave the ship.
-                if ( true === $upscale ) remove_filter( 'image_resize_dimensions', array( $this, 'aq_upscale' ) );
+						$resized_file = $editor->save();
 
-                // Return the output.
-                if ( $single ) {
-                    // str return.
-                    $image = $img_url;
-                } else {
-                    // array return.
-                    $image = array (
-                        0 => $img_url,
-                        1 => $dst_w,
-                        2 => $dst_h
-                    );
-                }
+						if ( ! is_wp_error( $resized_file ) ) {
+							$resized_rel_path = str_replace( $upload_dir, '', $resized_file['path'] );
+							$img_url          = $upload_url . $resized_rel_path;
+						} else {
+							throw new Aq_Exception( 'Unable to save resized image file: ' . $editor->get_error_message() );
+						}
+					}
 
-                return $image;
-            }
-            catch (Aq_Exception $ex) {
-                error_log('Aq_Resize.process() error: ' . $ex->getMessage());
+					/* Update image meta data on image creation to support 3rd party image optimization plugins */
 
-                if ($this->throwOnError) {
-                    // Bubble up exception.
-                    throw $ex;
-                }
-                else {
-                    // Return false, so that this patch is backwards-compatible.
-                    return false;
-                }
-            }
-        }
+					$attach_id = attachment_url_to_postid( $url );
+					if ( $attach_id ) {
+						// Get image metadata.
+						$meta = wp_get_attachment_metadata( $attach_id );
 
-        /**
-         * Callback to overwrite WP computing of thumbnail measures
-         */
-        function aq_upscale( $default, $orig_w, $orig_h, $dest_w, $dest_h, $crop ) {
-            if ( ! $crop ) return null; // Let the wordpress default function handle this.
+						// Set image metadata.
+						if ( $meta && is_array( $meta ) ) {
 
-            // Here is the point we allow to use larger image size than the original one.
-            $aspect_ratio = $orig_w / $orig_h;
-            $new_w = $dest_w;
-            $new_h = $dest_h;
+							if ( ! isset( $meta['sizes'] ) ) {
+								$meta['sizes'] = array();
+							}
 
-            if ( ! $new_w ) {
-                $new_w = intval( $new_h * $aspect_ratio );
-            }
+							$int_size = 'sq_' . $dst_w . 'x' . $dst_h;
 
-            if ( ! $new_h ) {
-                $new_h = intval( $new_w / $aspect_ratio );
-            }
+							if ( ! array_key_exists( $int_size, $meta['sizes'] )
+							     || ( $dst_w != $meta['sizes'][ $int_size ]['width'] || $dst_h != $meta['sizes'][ $int_size ]['height'] )
+							) {
 
-            $size_ratio = max( $new_w / $orig_w, $new_h / $orig_h );
+								// Check correct mime type.
+								$mime_type = wp_check_filetype( $img_url );
+								$mime_type = isset( $mime_type['type'] ) ? $mime_type['type'] : '';
 
-            $crop_w = round( $new_w / $size_ratio );
-            $crop_h = round( $new_h / $size_ratio );
+								// Cropped image file name.
+								$dst_filename = $info['filename'] . '-' . $suffix . $ext;
 
-            $s_x = floor( ( $orig_w - $crop_w ) / 2 );
-            $s_y = floor( ( $orig_h - $crop_h ) / 2 );
+								// Add cropped image to image meta.
+								$meta['sizes'][ $int_size ] = array(
+									'file'      => $dst_filename,
+									'width'     => $dst_w,
+									'height'    => $dst_h,
+									'mime-type' => $mime_type,
+									'aqua-sq'   => true,
+								);
 
-            return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
-        }
-    }
+								// Update meta.
+								update_post_meta( $attach_id, '_wp_attachment_metadata', $meta );
+							}
+						}
+					}
+				}
+
+				// Okay, leave the ship.
+				if ( true === $upscale ) {
+					remove_filter( 'image_resize_dimensions', array( $this, 'aq_upscale' ) );
+				}
+
+				// Return the output.
+				if ( $single ) {
+					// str return.
+					$image = $img_url;
+				} else {
+					// array return.
+					$image = array(
+						0 => $img_url,
+						1 => $dst_w,
+						2 => $dst_h,
+					);
+				}
+
+				return $image;
+			}
+			catch (Aq_Exception $ex) {
+				error_log('Aq_Resize.process() error: ' . $ex->getMessage());
+
+				if ($this->throwOnError) {
+					// Bubble up exception.
+					throw $ex;
+				}
+				else {
+					// Return false, so that this patch is backwards-compatible.
+					return false;
+				}
+			}
+		}
+
+		/**
+		 * Callback to overwrite WP computing of thumbnail measures
+		 */
+		function aq_upscale( $default, $orig_w, $orig_h, $dest_w, $dest_h, $crop ) {
+			if ( ! $crop ) {
+				return null;
+			} // Let the wordpress default function handle this.
+
+			// Here is the point we allow to use larger image size than the original one.
+			$aspect_ratio = $orig_w / $orig_h;
+			$new_w        = $dest_w;
+			$new_h        = $dest_h;
+
+			if ( ! $new_w ) {
+				$new_w = intval( $new_h * $aspect_ratio );
+			}
+
+			if ( ! $new_h ) {
+				$new_h = intval( $new_w / $aspect_ratio );
+			}
+
+			$size_ratio = max( $new_w / $orig_w, $new_h / $orig_h );
+
+			$crop_w = round( $new_w / $size_ratio );
+			$crop_h = round( $new_h / $size_ratio );
+
+			$s_x = floor( ( $orig_w - $crop_w ) / 2 );
+			$s_y = floor( ( $orig_h - $crop_h ) / 2 );
+
+			return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
+		}
+	}
 }
 
 
+if ( ! function_exists( 'aq_resize' ) ) {
 
+	/**
+	 * This is just a tiny wrapper function for the class above so that there is no
+	 * need to change any code in your own WP themes. Usage is still the same :)
+	 */
+	function aq_resize( $url, $width = null, $height = null, $crop = null, $single = true, $upscale = false ) {
+		$aq_resize = Aq_Resize::getInstance();
 
+		$original_url = $url;
 
-if(!function_exists('aq_resize')) {
+		/* WPML Fix */
+		if ( defined( 'ICL_SITEPRESS_VERSION' ) ) {
+			global $sitepress;
+			$url = $sitepress->convert_url( $url, $sitepress->get_default_language() );
 
-    /**
-     * This is just a tiny wrapper function for the class above so that there is no
-     * need to change any code in your own WP themes. Usage is still the same :)
-     */
-    function aq_resize( $url, $width = null, $height = null, $crop = null, $single = true, $upscale = false ) {
-        /* WPML Fix */
-        if ( defined( 'ICL_SITEPRESS_VERSION' ) ){
-            global $sitepress;
-            $url = $sitepress->convert_url( $url, $sitepress->get_default_language() );
-        }
-        /* WPML Fix */
+			if ( $url == false ) {
+				$url = $original_url;
+			}
+		}
+		/* WPML Fix */
 
-        $aq_resize = Aq_Resize::getInstance();
-        return $aq_resize->process( $url, $width, $height, $crop, $single, $upscale );
-    }
+		$response = $aq_resize->process( $url, $width, $height, $crop, $single, $upscale );
+		if ( $response == false ) {
+			return $original_url;
+		}
+
+		return $response;
+	}
 }
-
-
